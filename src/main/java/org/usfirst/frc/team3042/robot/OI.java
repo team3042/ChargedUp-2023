cb4bc838a777a6f46d26fc7ae8772f15cd1c568a
Anthony Brogni <anthonybrog@gmail.com>
2023-01-14 14:06:04 -0600

    updated oi.java
    got rid of stuff we didnt need and added x, y, z speeds

--- src/main/java/org/usfirst/frc/team3042/robot/OI.java
+++ src/main/java/org/usfirst/frc/team3042/robot/OI.java
@@ -1,17 +1,31 @@
 package org.usfirst.frc.team3042.robot;
 
-import org.usfirst.frc.team3042.lib.Log;
-import org.usfirst.frc.team3042.robot.commands.Drivetrain_XStance;
-import org.usfirst.frc.team3042.robot.subsystems.Drivetrain;
-import org.usfirst.frc.team3042.robot.RobotMap;
-
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj.GenericHID;
 
-public class OI {
+
+   
+
+/** OI ************************************************************************
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot. */
+public class OI { 
+   
     // some declarations
-    public static final GenericHID controller = new GenericHID(RobotMap.CONTROLLER_USB_PORT_ID); // Instantiate our controller at the specified USB port 
+     public static final GenericHID controller = new GenericHID(RobotMap.CONTROLLER_USB_PORT_ID); // Instantiate our controller at the specified USB port 
+
+    public double getYSpeed() {
+        return 0;
+    }
+
+    public double getXSpeed() {
+        return 0;
+    }
+
+    public double getZSpeed() {
+        return 0;
+    }
  
     // The rest of the class
     // blah
 }
+ 
