fe3f5e76e5073ce9256ed593af48cf64e11c1da5
ANC-2020 <75920228+ANC-2020@users.noreply.github.com>
2023-01-14 13:58:33 -0600

    Update Oi.java

--- src/main/java/org/usfirst/frc/team3042/robot/OI.java
+++ src/main/java/org/usfirst/frc/team3042/robot/OI.java
@@ -3,9 +3,15 @@ package org.usfirst.frc.team3042.robot;
 import org.usfirst.frc.team3042.lib.Log;
 import org.usfirst.frc.team3042.robot.commands.Drivetrain_XStance;
 import org.usfirst.frc.team3042.robot.subsystems.Drivetrain;
-import org.usfirst.frc.team3042.robot.Robot;
+import org.usfirst.frc.team3042.robot.RobotMap;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj.GenericHID;
 
-public static final GenericHID controller = new GenericHID(RobotMap.CONTROLLER_USB_PORT_ID) ; // Instantiate our controller at the specified USB port 
+public class OI {
+    // some declarations
+    public static final GenericHID controller = new GenericHID(RobotMap.CONTROLLER_USB_PORT_ID); // Instantiate our controller at the specified USB port 
+ 
+    // The rest of the class
+    // blah
+}
