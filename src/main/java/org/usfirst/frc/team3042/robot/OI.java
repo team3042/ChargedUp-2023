ecabcab55afad12b04a6186635fe018df81b0574
Anthony Brogni <anthonybrog@gmail.com>
2023-01-15 11:35:45 -0600

    Added code/examples for using Xbox Controllers to drive/gun

--- src/main/java/org/usfirst/frc/team3042/robot/OI.java
+++ src/main/java/org/usfirst/frc/team3042/robot/OI.java
@@ -5,6 +5,7 @@ import org.usfirst.frc.team3042.robot.commands.Drivetrain_XStance;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -18,6 +19,8 @@ public class OI {
     public static final GenericHID controller = new GenericHID(RobotMap.CONTROLLER_USB_PORT_ID); // Instantiate our controller at the specified USB port 
     public static final Joystick joyLeft = new Joystick(RobotMap.USB_JOYSTICK_LEFT); // Instantiate our left joystick at the specified USB port 
     public static final Joystick joyRight = new Joystick(RobotMap.USB_JOYSTICK_RIGHT); // Instantiate our right joystick at the specified USB port 
+	//public static final XboxController driverController = new XboxController(RobotMap.DRIVER_XBOX_USB_PORT); // TODO: Uncomment this to use an Xbox controller for the driver
+	//public static final XboxController gunnerController = new XboxController(RobotMap.GUNNER_XBOX_USB_PORT); // TODO: Uncomment this to use an Xbox controller for the gunner
 
 	/** OI ********************************************************************
 	 * Assign commands to the buttons and triggers*/
@@ -27,15 +30,21 @@ public class OI {
         // Bind commands to buttons on the controller/joysticks here! //
 		new Trigger(() -> joyLeft.getTrigger()).onTrue(new InstantCommand(Robot.drivetrain::zeroGyro, Robot.drivetrain)); // Zero the gyro, this is helpful at the start of a match for field-oriented driving
 		new Trigger(() -> joyRight.getTrigger()).onTrue(new Drivetrain_XStance()); // Defensive X-stance command
+		//new Trigger(() -> getLeftTrigger(driverController)).onTrue(new InstantCommand(Robot.drivetrain::zeroGyro, Robot.drivetrain)); // TODO: Uncomment this to use an Xbox controller for the driver
+		//new Trigger(() -> getRightTrigger(driverController)).onTrue(new Drivetrain_XStance()); // TODO: Uncomment this to use an Xbox controller for the driver
 
-        // Example using the A button on the controller below:
+        // Example using the A button on a generic logitech controller:
         // new Trigger(() -> controller.getRawButton(RobotMap.A_BUTTON)).onTrue(new Drivetrain_XStance());
+
+		// Example using the A button on a Xbox controller:
+        // new Trigger(() -> gunnerController.getAButton()).onTrue(new Drivetrain_XStance());
 	}
 
     /** Access to the driving axes values *****************************
 	 * A negative can be added to make pushing forward positive/negative. */
 	public double getXSpeed() {
 		double joystickValue = joyRight.getY();
+		//double joystickValue = driverController.getRightY(); // TODO: Uncomment this to use an Xbox controller for the driver
 		if (Math.abs(joystickValue) < 0.01) { // This is our deadband
 			return 0.0;
 		}
@@ -45,6 +54,7 @@ public class OI {
 	}
 	public double getYSpeed() {
 		double joystickValue = joyRight.getX();
+		//double joystickValue = driverController.getRightX(); // TODO: Uncomment this to use an Xbox controller for the driver
 		if (Math.abs(joystickValue) < 0.01) { // This is our deadband
 			return 0.0;
 		}
@@ -54,6 +64,7 @@ public class OI {
 	}
 	public double getZSpeed() {
 		double joystickValue = joyLeft.getX();
+		//double joystickValue = driverController.getLeftX(); // TODO: Uncomment this to use an Xbox controller for the driver
 		if (Math.abs(joystickValue) < 0.01) { // This is our deadband
 			return 0.0;
 		}
@@ -61,6 +72,14 @@ public class OI {
 			return joystickValue * RobotMap.kPhysicalMaxTurningSpeedRadiansPerSecond * -1; // Multiply by -1 reverses the direction
 		}	
 	}	
+
+	// These methods can be used to detect when the left/right trigger of a XboxController is pressed (convert the analog trigger axis to a boolean value: pressed or not pressed)
+	public boolean getLeftTrigger(XboxController controller) {
+		return controller.getLeftTriggerAxis() >= 0.95;
+	  }
+	public boolean getRightTrigger(XboxController controller) {
+		return controller.getRightTriggerAxis() >= 0.95;
+	}
  
     // The rest of the class
     // blah
