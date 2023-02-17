87c22ca82c6658e6dd887f4b1ecd4c005fc5bd33
Anthony Brogni <anthonybrog@gmail.com>
2023-02-17 16:20:04 -0600

    Added conditions

--- src/main/java/org/usfirst/frc/team3042/robot/commands/Arm_SetPosition.java
+++ src/main/java/org/usfirst/frc/team3042/robot/commands/Arm_SetPosition.java
@@ -13,16 +13,16 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class Arm_SetPosition extends CommandBase {
   Arm arm = Robot.arm;
 
-  public double rotationPosition;
-  public double extensionPosition;
+  public double rotationPositionGoal;
+  public double extensionPositionGoal;
 
   /** Creates a new Arm_SetPosition command. */
   public Arm_SetPosition(double rotationGoal, double extensionPercent) { // rotationGoal is measured in encoder counts
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(arm);
 
-    this.rotationPosition = rotationGoal;
-    this.extensionPosition = extensionPercent * RobotMap.maxArmLength; // convert from a percent to actual encoder counts
+    this.rotationPositionGoal = rotationGoal;
+    this.extensionPositionGoal = extensionPercent * RobotMap.maxArmLength; // convert from a percent to actual encoder counts
   }
 
   // Called when the command is initially scheduled.
@@ -32,17 +32,32 @@ public class Arm_SetPosition extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    double rotationError = arm.getRotationMotorPosition() - rotationPositionGoal;
+    double extensionError = arm.getExtendMotorPosition() - extensionPositionGoal;
 
-    double rotationError = arm.getRotationMotorPosition() - rotationPosition;
-    double minimalVoltage = RobotMap.levelVoltage * Math.cos(arm.getArmAngle() * (arm.getExtendMotorPosition()/RobotMap.maxArmLength));
-    arm.setVoltageRotationMotor(minimalVoltage + (rotationError * RobotMap.rotation_kP)); 
+    boolean extensionGoalReached = (Math.abs(extensionError) <= RobotMap.extensionThreshold);
+    boolean inIntakePosition = Math.abs(arm.getRotationMotorPosition() - RobotMap.kIntakeArmPosition) <= RobotMap.rotationThreshold;
 
-    double extensionError = arm.getExtendMotorPosition() - extensionPosition;
-    if (Math.abs(extensionError) > RobotMap.extensionThreshold){
-      arm.setPowertoExtend(Math.copySign(0.2, extensionError)); // increase percent power to make arm move faster
-      // if it keeps oscillating because it can't reach the correct position, use (extensionError * kP) instead
-    } else {
-      arm.stopExtendMotor();
+    // When going to the intake position, we will wait for the extension to move first before rotating the arm
+    if (rotationPositionGoal != RobotMap.kIntakeArmPosition || (rotationPositionGoal == RobotMap.kIntakeArmPosition && extensionGoalReached)) {
+
+      // THIS BLOCK OF CODE BELOW ROTATES THE ARM SHOULDER //
+      double minimalVoltage = RobotMap.levelVoltage * Math.cos(arm.getArmAngle() * (arm.getExtendMotorPosition()/RobotMap.maxArmLength));
+      arm.setVoltageRotationMotor(minimalVoltage + (rotationError * RobotMap.rotation_kP)); 
+
+    }
+    
+    // We are only going to move the extension if the arm is NOT in the intake position
+    if (!inIntakePosition) {
+
+      // THIS BLOCK OF CODE BELOW MOVES THE EXTENSION //
+      if (!extensionGoalReached) {
+        arm.setPowertoExtend(Math.copySign(0.2, extensionError)); // TODO: increase percent power to make arm move faster
+        // if it keeps oscillating because it can't reach the correct position, use (extensionError * kP) instead
+      } else {
+        arm.stopExtendMotor();
+      }
+
     }
   }
 
