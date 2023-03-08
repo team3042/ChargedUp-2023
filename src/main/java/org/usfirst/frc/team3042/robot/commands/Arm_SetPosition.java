b2413dde0cfffef65c609684e70a754f2889e41c
Anthony Brogni <anthonybrog@gmail.com>
2023-03-08 10:01:02 -0600

    Delted an empty IF statement I found

--- src/main/java/org/usfirst/frc/team3042/robot/commands/Arm_SetPosition.java
+++ src/main/java/org/usfirst/frc/team3042/robot/commands/Arm_SetPosition.java
@@ -28,10 +28,7 @@ public class Arm_SetPosition extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-
-    if (rotationPositionGoal >= RobotMap.kHighArmPosCone){
-      
-    }
+    
   }
 
   // Called every time the scheduler runs while the command is scheduled.
