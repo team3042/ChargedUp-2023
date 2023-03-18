77853ff25229a97d05f87ef9b8bc1cc3c6d8d1c2
Anthony Brogni <anthonybrog@gmail.com>
2023-03-18 15:12:26 -0500

    Little bit of clean up

--- src/main/java/org/usfirst/frc/team3042/robot/commands/autonomous/Balance_On_Station.java
+++ src/main/java/org/usfirst/frc/team3042/robot/commands/autonomous/Balance_On_Station.java
@@ -14,7 +14,6 @@ public class Balance_On_Station extends CommandBase {
 
   Drivetrain drivetrain = Robot.drivetrain;
   public boolean moving_fast = true;
-  public boolean overshot = false;
 
   /** Creates a new Balance_On_Station command. */
   public Balance_On_Station() {
@@ -31,23 +30,18 @@ public class Balance_On_Station extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    
+		// System.out.println(moving_fast); // Useful print statement for debugging
+
     // When we get onto the charging station, slow down but continue driving forward
-
-		// System.out.println(moving_fast);
-
     if(moving_fast == true && drivetrain.pitchAngle() <= -8) {
-
       System.out.println("in if");
       moving_fast = false;
-
     }
     
     if(moving_fast == false) {
-
-      drivetrain.drive(-0.3,0,0,false); 
-
+      drivetrain.drive(-0.3,0,0,false); // Move forward slowly using the drive() method
     } else if (moving_fast == true){
-
       drivetrain.drive(-1, 0, 0, false); // Move forward fast using the drive() method
     }
   }
